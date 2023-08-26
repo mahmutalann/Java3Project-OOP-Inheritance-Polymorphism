44ecb9a40a4c49862edb7d8a2cbbf75b58c710e3
Mahmut <56754759+mahmutalann@users.noreply.github.com>
2023-08-27 01:14:48 +0300

    OOP-Inheritance-Polymorphism

--- src/inheritance/IndividualCustomer.java
+++ src/inheritance/IndividualCustomer.java
@@ -1,6 +1,6 @@
 package inheritance;
 
-public class IndividualCustomer extends Customer {
+public class IndividualCustomer extends Customer { //extends provides that allows the individual class to use the attributes in the customer class.
 	String firstName;
 	String lastName;
 	String nationalIdentity;
